66272edd62ef2837612e8123c91624c9d50535fa
ngminteck <ng_min_teck@hotmail.com>
2021-10-07 18:43:48 +0800

    add source
    add source

---
+++ OpenGLLearning/OpenGL/DirectionalLight.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Light.h"
+
+class DirectionalLight :
+	public Light
+{
+public:
+	DirectionalLight();
+	DirectionalLight(GLuint shadowWidth, GLuint shadowHeight, 
+					GLfloat red, GLfloat green, GLfloat blue,
+					GLfloat aIntensity, GLfloat dIntensity,
+					GLfloat xDir, GLfloat yDir, GLfloat zDir);
+
+	void UseLight(GLuint ambientIntensityLocation, GLuint ambientColourLocation,
+		GLuint diffuseIntensityLocation, GLuint directionLocation);
+
+	glm::mat4 CalculateLightTransform();
+
+	~DirectionalLight();
+
+private:
+	glm::vec3 direction;
+};
+
